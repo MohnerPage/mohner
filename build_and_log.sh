655a9e65d6d1358ebf051ead4b7ed38c84e744bf
Rubén Oroz <macbookangular@Macbook-de-Ruben-Oroz.local>
2026-03-30 14:57:28 -0600

    chore: remove sensitive/dev files (openclaw, agent configs, test scripts)

--- build_and_log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd "/Users/univa/Documents/web_envases"
-echo "=== Build started at $(date) ===" > build.log
-npm run build 2>&1 | tee -a build.log
-echo "=== Build finished at $(date) ===" >> build.log
